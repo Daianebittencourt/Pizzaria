43e5340a9f63ad0756ec930df9194e08b8392d51
Daiane Bittencourt da Silva <daiane.silva@ubicua.com.br>
2023-06-16 15:46:12 -0300

    Criação da SRC com os arquivos routes e server

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response} from "express";
+
+const router = Router();
+
+router.get('/teste', (req: Request, res: Response) => {
+//return res.json({nome: "Pizzaria"})
+throw new Error('Erro ao fazer essa requisição!')
+
+
+})
+
+export{ router };
