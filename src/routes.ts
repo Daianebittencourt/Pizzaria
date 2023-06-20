950b775564cbe25333e9ac54dfbb88efee10532e
Daiane Bittencourt da Silva <daiane.silva@ubicua.com.br>
2023-06-20 12:07:53 -0300

    Gerando Token JWT

--- src/routes.ts
+++ src/routes.ts
@@ -3,14 +3,16 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
-const createUserController = CreateUserController; // verificar se está correto, mas está rodando e as rotas estão funcionando 
-const authUserController = AuthUserController;
+//const createUserController = CreateUserController; // verificar se está correto, mas está rodando e as rotas estão funcionando 
+//const authUserController = AuthUserController;
+const authUserController = new AuthUserController();
+const createUserController = new CreateUserController();
 
 //rotas USERS 
 //bate na rota /users -> chama o controller
-router.post('/users', new createUserController().handle) //instânciando o controller | handle para que serve?
- 
+//router.post('/users', new createUserController().handle) //instânciando o controller | handle para que serve?
+ router.post('/users', createUserController.handle)
 //rota para logar usuário
-router.post('/login', new authUserController().handle)
+router.post('/login', authUserController.handle)
 
 export{ router };
---
+++ src/services/user/DetailUserService.ts
@@ -0,0 +1,9 @@
+import prismaClient from "../../prisma";
+
+class DetailUSerService{ // detalhes do usuário 
+    async execute(){
+
+    }
+}
+
+export {DetailUSerService} //quando usar o "export default DetailUSerService;"
